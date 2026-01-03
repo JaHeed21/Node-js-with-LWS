b1a9f588dac917d78582ba38a23302f4f4604728
JaHeed21 <jahidrayhan100@gmail.com>
2026-01-03 20:55:13 +0600

    purse the url for basic routes

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const http = require("http");
+const url = require("url");
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from your basic Node.js project\n");
-});
+function handleRequest(req, res) {
+  const { method } = req;
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const trimmedPathname = pathname.replace(/\/+$/, "") || "/";
+  console.log("Parsed URL:", parsedUrl);
+  console.log("Trimmed pathname:", trimmedPathname);
+
+  if (method === "GET" && trimmedPathname === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Home page");
+  } else if (method === "GET" && trimmedPathname === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About page");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not found");
+  }
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
